e7557bb83366bcfa6e86283e50caf8fd5988f1da
Digi85 <73599904+Digi85@users.noreply.github.com>
2021-06-08 23:46:56 -0500

    first

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    alert("hola");
+})
+
+$(document).ready(function(){
+    mensaje();
+})
+
+function mensaje(){
+    alert("Bienvenido a jquery");
+}
+function mensaje(){
+    $("p").css({
+        color:"white",
+        background:"black"
+    })
+}
+function a(){
+    $('#parrafo').text("hola mundo");
+}
+$(document)  
